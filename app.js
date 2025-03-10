97957f1e4db8a738f5d35273171e52bd18a739c4
DaisukeSimmons <davidsimmons1414@gmail.com>
2025-03-10 17:10:43 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Daisuke knows AWS'));
+app.get('/', (req, res) => res.send('Daisuke knows AWS CodeBuild and CodePipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
